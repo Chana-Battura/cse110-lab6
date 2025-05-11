975c4fef0ce6d9395e6fcbf93657389d42e923c7
Charan Battula <charanb2005@gmail.com>
2025-05-11 14:31:48 -0700

    Finished Explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -41,12 +41,14 @@ function addRecipesToDocument(recipes) {
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
-	for (let i = 0; i<recipes.length; i++){
-		let recipeCard = document.createElement('recipe-card');
-		recipeCard.data = recipes[i];
-		mainRef.append(recipeCard);
+	if (recipes){
+		for (let i = 0; i<recipes.length; i++){
+			let recipeCard = document.createElement('recipe-card');
+			recipeCard.data = recipes[i];
+			mainRef.append(recipeCard);
+		}
 	}
-
+	return
 }
 
 /**
@@ -59,6 +61,7 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+	localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -67,21 +70,46 @@ function saveRecipesToStorage(recipes) {
  */
 function initFormHandler() {
 	// B2. TODO - Get a reference to the <form> element
+	let recFrom = document.querySelector("form");
 	// B3. TODO - Add an event listener for the 'submit' event, which fires when the
 	//            submit button is clicked
-	// Steps B4-B9 will occur inside the event listener from step B3
-	// B4. TODO - Create a new FormData object from the <form> element reference above
-	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
-	//            make this easier to read), and then extract the keys and corresponding
-	//            values from the FormData object and insert them into recipeObject
-	// B6. TODO - Create a new <recipe-card> element
-	// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
-	// B8. TODO - Append this new <recipe-card> to <main>
-	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
-	//            then save the recipes array back to localStorage
+	recFrom.addEventListener("submit", (event) => {
+		// Steps B4-B9 will occur inside the event listener from step B3
+		// B4. TODO - Create a new FormData object from the <form> element reference above
+		let recData = new FormData(recFrom);
+		// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
+		//            make this easier to read), and then extract the keys and corresponding
+		//            values from the FormData object and insert them into recipeObject
+		let recipeObject = {};
+		for (const key of recData){
+			recipeObject[key[0]] = key[1];
+		}
+		// B6. TODO - Create a new <recipe-card> element
+		let recipeCard = document.createElement('recipe-card');
+		// B7. TODO - Add the recipeObject data to <recipe-card> using element.data
+		recipeCard.data = recipeObject;
+		// B8. TODO - Append this new <recipe-card> to <main>
+		let mainRef = document.querySelector('main');
+		mainRef.append(recipeCard);
+		// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
+		//            then save the recipes array back to localStorage
+		let recipes = getRecipesFromStorage();
+		if (recipes){
+			recipes.push(recipeObject)
+		} else {
+			recipes = [recipeObject];
+		}
+		saveRecipesToStorage(recipes);
+	});
 	// B10. TODO - Get a reference to the "Clear Local Storage" button
+	let clearBtn = document.querySelector("button.danger");
 	// B11. TODO - Add a click event listener to clear local storage button
-	// Steps B12 & B13 will occur inside the event listener from step B11
-	// B12. TODO - Clear the local storage
-	// B13. TODO - Delete the contents of <main>
+	clearBtn.addEventListener("click", () => {
+		// Steps B12 & B13 will occur inside the event listener from step B11
+		// B12. TODO - Clear the local storage
+		localStorage.clear();
+		// B13. TODO - Delete the contents of <main>
+		document.querySelector("main").innerHTML = "";
+	});
+	
 }
